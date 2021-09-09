e8a091369e41950337d7a8113b9e0209d19b0bb9
Jason Wildermuth <jmw33322@hotmail.com>
2021-09-09 04:54:13 -0400

    Fix anchor jumps and burger menu

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let menu = document.querySelector(".nav-list")
 let toggle = document.querySelector(".burger-toggle")
 
 window.addEventListener('mouseup', function(event){
-  if (event.target != toggle && event.target != menu && event.target.parentNode != menu){
+  if (event.target != toggle){
         toggle.checked = false;
     }
 });
-
